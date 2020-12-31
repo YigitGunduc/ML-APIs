0f6ffec6d7ad50465bda1dc4319a13a2c5591e69
YigitGunduc <yy347112@gmail.com>
2020-12-31 21:17:01 +0300

    returns value with positions

--- ai/vision/QRCodeDetectionLib.py
+++ ai/vision/QRCodeDetectionLib.py
@@ -4,8 +4,17 @@ from pyzbar.pyzbar import decode
 
 
 def QRCodeDetection(imFile):
+			
+	result = dict() 
+
 	img = cv2.imread(imFile)
 
 	for barcode in decode(img):
-		myData = barcode.data.decode('utf-8')
-		return(myData)
+		data = barcode.data.decode('utf-8')
+		points = np.array(barcode.polygon,dtype=np.int32)
+		
+		corners = tuple(map(tuple, points))
+		result['Corners'] = str(corners)[1:-1]
+		result['Value'] = data
+		
+		return result
